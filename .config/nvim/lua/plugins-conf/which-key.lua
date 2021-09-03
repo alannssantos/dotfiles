e821cb78e22042e1f9b732aecac8908e651024cf
Alann Santos <santos_alann@hotmail.com>
2021-09-03 08:00:13 -0300

    Update which-key.lua

--- .config/nvim/lua/plugins-conf/which-key.lua
+++ .config/nvim/lua/plugins-conf/which-key.lua
@@ -1,14 +1,15 @@
 require'which-key'.setup()
 
 require'which-key'.register({
-  ["<Tab>"] = { "<cmd>bn<CR>", "Fechar Guia" },
-  ["<leader>q"] = { "<cmd>bw<CR>", "Fechar Guia" },
+  ["<Tab>"] = { "<cmd>tabn<CR>", "Próxima Guia" },
+  ["<leader>q"] = { "<cmd>q<CR>", "Fechar Guia" },
   ["<leader>/"] = { "<cmd>CommentToggle<CR>", "Comentar" },
   ["<leader>e"] = { "<cmd>NvimTreeToggle<CR>", "Explorer" },
   ["<leader>f"] = { "<cmd>FzfLua files<cr>", "Procurar Arquivo" },
   ["<leader>o"] = { "<cmd>set spell!<CR>", "Corretor Ortográfico" },
   ["<leader>n"] = { "<cmd>%!nl -n rz -w3<CR>", "Numerador de Linhas" },
   ["<leader>s"] = { "<cmd>!clear && shellcheck -x %<CR>", "ShellCheck" },
+  ["<leader>t"] = { "<cmd>tab sball<CR>", "Transformar buffers em tabs" },
 })
 
 vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {})
