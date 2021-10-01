307c6f7563002005bc6e7964ab20339b25ad9d4d
Alann Santos <santos_alann@hotmail.com>
2021-10-01 10:27:14 -0300

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -13,14 +13,10 @@ return require('packer').startup(function()
   use 'norcalli/nvim-colorizer.lua'
   use 'kyazdani42/nvim-web-devicons'
   -- Endentação.
-  use { 'vimwiki/vimwiki',
-    config = vim.cmd([[let g:vimwiki_key_mappings = { 'all_maps': 0, }]])
-  }
+  use 'vimwiki/vimwiki'
   use 'windwp/nvim-autopairs'
   use 'terrortylor/nvim-comment'
   -- Gerenciamento de Arquivo.
-  use 'lambdalisue/suda.vim'
-  use 'kyazdani42/nvim-tree.lua'
   use { 'ibhagwan/fzf-lua',
     requires = {'vijaymarupudi/nvim-fzf'}
   }
