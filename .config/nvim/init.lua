9eb91c5ffe8bb4040667b2bda2c346d04692fa0b
Alann Santos <santos_alann@hotmail.com>
2022-09-14 07:13:18 -0300

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,12 +4,30 @@
 --  / ___ \| | (_| | | | | | | |
 -- /_/   \_\_|\__,_|_| |_|_| |_|
 
-require('Packer')
-require('Config_Basicas')
-require('Config_Plugins.glow')
-require('Config_Plugins.comment')
-require('Config_Plugins.nvim-cmp')
-require('Config_Plugins.autopairs')
-require('Config_Plugins.colorizer')
-require('Config_Plugins.which-key')
-require('Config_Plugins.lsp-config')
+-- Leader key
+vim.g.mapleader=' '
+
+vim.o.path='+=**'
+vim.o.hidden=true
+-- vim.wo.spell=true
+vim.wo.number=true
+vim.o.confirm=true
+vim.o.laststatus=2
+vim.o.shiftwidth=2
+vim.o.hlsearch=true
+vim.o.showtabline=2
+vim.o.softtabstop=2
+vim.o.wildmenu=true
+vim.o.spelllang='pt'
+vim.o.expandtab=true
+vim.o.incsearch=true
+vim.o.splitbelow=true
+vim.o.splitright=true
+vim.o.smartindent=true
+vim.o.foldmethod='marker'
+vim.o.clipboard='unnamedplus'
+
+vim.g.netrw_altv=1
+vim.g.netrw_winsize=25
+vim.g.netrw_liststyle=3
+vim.g.netrw_browse_split=3
