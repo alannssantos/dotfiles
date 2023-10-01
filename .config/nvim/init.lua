9f278e33eacd4e5ecb51149c9afb78a4414aa0cd
Alann Santos <santos_alann@hotmail.com>
2023-10-01 20:07:16 -0300

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -35,7 +35,7 @@ vim.opt.guifont         = 'Fira Code:h12'
 -- mapping
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("i", "kj", "<Esc>")
-vim.keymap.set('n', '<Tab>', ':tabn<CR>')
+vim.keymap.set("n", "<Tab>", ":bNext<CR>")
 vim.keymap.set('n', '<leader>e', ':Texplore<CR>')
 vim.keymap.set('n', '<leader>n', ':%!nl -n rz -w3<CR>')
 vim.keymap.set('n', '<leader>o', ':set spell! spelllang=pt<CR>')
