253f39cb761b98821ffcd86518642948eb5d696f
Alann Santos <santos_alann@hotmail.com>
2024-03-04 07:39:05 -0300

    Update config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -32,86 +32,91 @@
 
 from typing import List
 
-import os
+import os, random
 import subprocess
-from libqtile.command import lazy
-from libqtile import qtile, layout, bar, widget, hook
+from libqtile.lazy import lazy
+from libqtile import layout, bar, widget, hook
 from libqtile.config import Key, Screen, Group, Drag, Click, Match
 
+from qtile_extras import widget
+from qtile_extras.widget.decorations import BorderDecoration
+
 # Variaveis.
-mod = "mod4"  # Escolher tecla Super.
+mod = 'mod4'  # Escolher tecla Super.
 home = os.path.expanduser('~')  # Definindo caminho Home.
 # Definir terminal padrão.
-terminal = "xfce4-terminal --hide-menubar --hide-toolbar"
+terminal = 'xfce4-terminal --hide-menubar --hide-toolbar'
+# Papel de Parede.
+wallhaven = os.path.expanduser('~') + '/.local/share/backgrounds/wallhaven/'
 
 # Cores.
-cor1 = "#8ee269"  # Cor Principal.
-cor2 = "#d3d0c8"  # Cor de Fonts.
-cor3 = "#2d2d2d"  # Cor de Background.
-cor4 = "#747369"  # Cor de janelas inativas.
-cor5 = "#f2777a"  # Cor de Alerta.
-
-# Cria Poll Funcion do Shell no bar com GenPollText.
-# def torrent():
-# return subprocess.check_output([home + "/.config/qtile/scripts/torrent"]).decode('utf-8').strip()
-
+Color = [
+    ['#2D353B','#2D353B'], #bg
+    ['#D3C6AA','#D3C6AA'], #fg
+    ['#E67E80','#E67E80'], #color01
+    ['#E69875','#E69875'], #color02
+    ['#DBBC7F','#DBBC7F'], #color03
+    ['#A7C080','#A7C080'], #color04
+    ['#83C092','#83C092'], #color05
+    ['#7FBBB3','#7FBBB3'], #color06
+    ['#D699B6','#D699B6'], #color07
+]
 
 keys = [
     # Controle de Som.
-    Key([], "XF86AudioRaiseVolume", lazy.spawn(
-            "amixer -q -D pulse sset Master 5%+")),
-    Key([], "XF86AudioLowerVolume", lazy.spawn(
-            "amixer -q -D pulse sset Master 5%-")),
-    Key([], "XF86AudioMute", lazy.spawn(
-            "amixer -q -D pulse sset Master toggle")),
+    Key([], 'XF86AudioRaiseVolume', lazy.spawn(
+            'amixer -q -D pulse sset Master 5%+')),
+    Key([], 'XF86AudioLowerVolume', lazy.spawn(
+            'amixer -q -D pulse sset Master 5%-')),
+    Key([], 'XF86AudioMute', lazy.spawn(
+            'amixer -q -D pulse sset Master toggle')),
 
     # Switch between windows in current stack pane
-    Key([mod], "Right", lazy.layout.down()),
-    Key([mod], "Left", lazy.layout.up()),
+    Key([mod], 'Right', lazy.layout.down()),
+    Key([mod], 'Left', lazy.layout.up()),
 
     # Move windows up or down in current stack
-    Key([mod, "control"], "Right", lazy.layout.shuffle_down()),
-    Key([mod, "control"], "Left", lazy.layout.shuffle_up()),
+    Key([mod, 'control'], 'Right', lazy.layout.shuffle_down()),
+    Key([mod, 'control'], 'Left', lazy.layout.shuffle_up()),
 
     # Switch window focus to other pane(s) of stack
-    # Key([mod], "space", lazy.layout.next()),
+    # Key([mod], 'space', lazy.layout.next()),
 
     # Swap panes of split stack
-    Key([mod, "shift"], "space", lazy.layout.rotate()),
+    Key([mod, 'shift'], 'space', lazy.layout.rotate()),
 
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split()),
-    Key([mod], "space", lazy.spawn(
-        home + "/.config/qtile/scripts/rofi-script")),
-    Key([mod], "Return", lazy.spawn(terminal)),
-    Key([], "Print", lazy.spawn("xfce4-screenshooter -f")),
-    Key(["mod1"], "Print", lazy.spawn("xfce4-screenshooter -r")),
-    Key([mod], "1", lazy.spawn("thunar")),
-    Key([mod], "2", lazy.spawn("firefox-esr")),
+    Key([mod, 'shift'], 'Return', lazy.layout.toggle_split()),
+    Key([mod], 'Return', lazy.spawn(terminal)),
+    Key([mod], 'Space', lazy.spawn('rofi -show drun')),
+    Key([], 'Print', lazy.spawn('xfce4-screenshooter -f')),
+    Key(['mod1'], 'Print', lazy.spawn('xfce4-screenshooter -r')),
+    Key([mod], '1', lazy.spawn('thunar')),
+    Key([mod], '2', lazy.spawn('firefox-esr')),
 
     # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout()),
-    Key([mod], "w", lazy.window.kill()),
-    Key([mod, "control"], "r", lazy.restart()),
-    Key([mod, "control"], "q", lazy.shutdown()),
-    Key([mod], "r", lazy.spawncmd()),
+    Key([mod], 'Tab', lazy.next_layout()),
+    Key([mod], 'w', lazy.window.kill()),
+    Key([mod, 'control'], 'r', lazy.restart()),
+    Key([mod, 'control'], 'q', lazy.shutdown()),
+    Key([mod], 'r', lazy.spawncmd()),
 ]
 
 groups = [
-    Group('a', matches=[Match(wm_class='Navigator'),
-                        Match(wm_class='Firefox-esr')]),
-    Group('s', matches=[Match(wm_class='Steam')]),
+    Group('a', matches = [Match(wm_class = 'Navigator'),
+                          Match(wm_class = 'Firefox-esr')]),
+    Group('s', matches = [Match(wm_class = 'Steam')]),
     Group('d'),
     Group('f'),
     Group('u'),
-    Group('i', matches=[Match(wm_class='qbittorrent'),
-                        Match(wm_class='qBittorrent')]),
+    Group('i', matches = [Match(wm_class = 'qbittorrent'),
+                          Match(wm_class = 'qBittorrent')]),
     Group('o'),
-    Group('p', matches=[Match(wm_class='calibre-gui'),
-                        Match(wm_class='telegram-desktop')]),
+    Group('p', matches = [Match(wm_class = 'calibre-gui'),
+                          Match(wm_class = 'telegram-desktop')]),
 ]
 
 for i in groups:
@@ -120,171 +125,188 @@ for i in groups:
         Key([mod], i.name, lazy.group[i.name].toscreen()),
 
         # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
+        Key([mod, 'shift'], i.name, lazy.window.togroup(i.name)),
     ])
 
 layouts = [
     layout.MonadTall(
-        border_focus=cor1,
-        border_normal=cor4,
-        margin=2,
+        margin = 2,
+        border_focus = Color[6],
+        border_normal = Color[0],
     ),
     layout.Max(),
 ]
 
 widget_defaults = dict(
-    font='Fira Mono',
-    fontsize=14,
-    padding=3,
+    padding = 3,
+    fontsize = 14,
+    font = 'Fira Mono',
+    background = Color[0],
 )
 extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
+        top = bar.Bar(
             [
-                widget.Image(
-                    scale=True,
-                    filename='~/.config/qtile/icons/monobar1.png',
-                ),
-                widget.Image(
-                    scale=True,
-                    filename='~/.config/qtile/icons/launcher.png',
-                    mouse_callbacks={
-                        'Button1': lambda: qtile.cmd_spawn(home + '/.config/qtile/scripts/rofi-script')},
-                    background=cor3,
-                ),
-                widget.Image(
-                    scale=True,
-                    filename='~/.config/qtile/icons/monobar4.png',
-                ),
-                widget.Prompt(
-                    prompt='Comando: ',
-                    font='Fira Mono',
-                    padding=10,
-                    foreground=cor1,
-                    background=cor3,
-                ),
                 widget.GroupBox(
-                    disable_drag='true',
-                    background=cor3,
-                    borderwidth=2,
-                    active=cor2,
-                    inactive=cor1,
-                    urgent_border=cor5,
-                    highlight_method='border',
-                    this_current_screen_border=cor1,
-                    other_screen_border=cor3,
+                    disable_drag = 'true',
+                    active = Color[6],
+                    inactive = Color[1],
+                    background = Color[0],
+                    urgent_border = Color[2],
+                    other_screen_border = Color[3],
+                    this_current_screen_border = Color[6],
+                    highlight_method = 'border',
+                    borderwidth = 2,
                 ),
-                widget.Image(
-                    scale=True,
-                    filename='~/.config/qtile/icons/monobar2.png',
+                widget.Spacer(length = 4),
+                widget.Prompt(
+                    padding = 10,
+                    prompt = 'Comando: ',
+                    foreground = Color[1],
+                    background = Color[0],
+                    decorations = [
+                    BorderDecoration(
+                         colour = Color[8],
+                         border_width = [0, 0, 2, 0],
+                     )
+                    ]
                 ),
+                widget.Spacer(length = 4),
                 widget.WindowName(
-                    font='Fira Mono',
-                    padding=10,
-                    foreground=cor3,
-                    background=cor1,
+                    padding = 10,
+                    background = Color[6],
+                    foreground = Color[0],
+                    decorations = [
+                    BorderDecoration(
+                         colour = Color[0],
+                         border_width = [2, 2, 2, 2],
+                     )
+                    ]
                 ),
-                widget.Image(
-                    scale=True,
-                    filename='~/.config/qtile/icons/monobar3.png',
+                widget.Spacer(length = 4),
+                widget.Net(
+                    format='{down:.0f}{down_suffix} ↓↑ {up:.0f}{up_suffix}',
+                    background = Color[0],
+                    foreground = Color[3],
+                    decorations = [
+                    BorderDecoration(
+                         colour = Color[3],
+                         border_width = [0, 0, 2, 0],
+                     )
+                    ]
                 ),
+                widget.Spacer(length = 8),
                 widget.Systray(
-                    background=cor3,
-                    padding=1,
+                    background = Color[0],
+                    padding = 1,
+                    decorations = [
+                    BorderDecoration(
+                         colour = Color[8],
+                         border_width = [0, 0, 2, 0],
+                     )
+                    ]
                 ),
-                widget.Image(
-                    scale=True,
-                    filename='~/.config/qtile/icons/monobar4.png',
-                ),
+                widget.Spacer(length = 4),
                 # widget.GenPollText(
-                # font='fira Sans',
-                # fontsize=14,
-                # func=torrent,
-                # update_interval=1,
-                # background=cor3,
-                # foreground=cor1,
+                # font = 'fira Sans',
+                # fontsize = 14,
+                # func = torrent,
+                # update_interval = 1,
+                # background = Color[1],
+                # foreground = Color[1],
                 # ),
                 widget.Volume(
-                    device='pulse',
-                    channel='Master',
-                    background=cor3,
-                    foreground=cor1,
-                    emoji=True,
+                    device = 'pulse',
+                    channel = 'Master',
+                    fmt = 'Vol: {}',
+                    background = Color[0],
+                    foreground = Color[4],
+                    decorations = [
+                    BorderDecoration(
+                         colour = Color[4],
+                         border_width = [0, 0, 2, 0],
+                     )
+                    ]
                 ),
+                widget.Spacer(length = 8),
                 widget.Battery(
-                    background=cor3,
-                    foreground=cor1,
-                    low_foreground=cor5,
-                    show_short_text=False,
-                    low_percentage=0.25,
-                    format=' {char} {percent:2.0%}',
-                    charge_char="",
-                    empty_char="",
-                    discharge_char="",
-                    full_char=" ",
-                    battery=0,
-                    update_interval=10,
+                    background = Color[0],
+                    foreground = Color[7],
+                    low_foreground = Color[2],
+                    low_percentage = 0.25,
+                    show_short_text = False,
+                    full_char = '  ',
+                    empty_char = '  ',
+                    charge_char = '  ',
+                    discharge_char = '  ',
+                    format = '{char}  {percent:2.0%}',
+                    battery = 0,
+                    update_interval = 10,
+                    decorations = [
+                    BorderDecoration(
+                         colour = Color[7],
+                         border_width = [0, 0, 2, 0]
+                     )
+                 ],
                 ),
-                widget.Image(
-                    scale=True,
-                    filename='~/.config/qtile/icons/monobar4.png',
-                ),
+                widget.Spacer(length = 8),
                 widget.Clock(
-                    background=cor3,
-                    foreground=cor1,
-                    font='Fira Mono',
-                    format='%a, %d de %b, %H:%M'
-                ),
-                widget.Image(
-                    scale=True,
-                    filename='~/.config/qtile/icons/monobar5.png',
+                    background = Color[0],
+                    foreground = Color[5],
+                    format = '%a, %d de %b, %H:%M',
+                    decorations = [
+                    BorderDecoration(
+                         colour = Color[5],
+                         border_width = [0, 0, 2, 0]
+                     )
+                 ],
                 ),
             ],
-            29,
+            26,
         ),
+        wallpaper_mode='fill',
+        wallpaper = wallhaven + random.choice(os.listdir(wallhaven))
     ),
 ]
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Drag([mod], 'Button1', lazy.window.set_position_floating(),
+         start = lazy.window.get_position()),
+    Drag([mod], 'Button3', lazy.window.set_size_floating(),
+         start = lazy.window.get_size()),
+    Click([mod], 'Button2', lazy.window.bring_to_front())
 ]
 
-dgroups_key_binder = None
-dgroups_app_rules: List = []
 main = None
-follow_mouse_focus = True
-bring_front_click = False
 cursor_warp = False
+bring_front_click = False
+dgroups_key_binder = None
+follow_mouse_focus = True
+dgroups_app_rules: List = []
 floating_layout = layout.Floating(
-    border_focus=cor1,
-    border_normal=cor4,
-    border_width=2,
+    border_width = 2,
+    border_focus = Color[1],
+    border_normal = Color[1],
 
     float_rules=[
         *layout.Floating.default_float_rules,
-        Match(wm_class='maketag'),  # gitk
-        Match(wm_class='makebranch'),  # gitk
-        Match(wm_class='ssh-askpass'),  # ssh-askpass
-        Match(wm_class='confirmreset'),  # gitk
-        Match(title='pinentry'),  # GPG key password entry
-        Match(title='branchdialog'),  # gitk
+        Match(wm_class = 'maketag'),  # gitk
+        Match(wm_class = 'makebranch'),  # gitk
+        Match(wm_class = 'ssh-askpass'),  # ssh-askpass
+        Match(wm_class = 'confirmreset'),  # gitk
+        Match(title = 'pinentry'),  # GPG key password entry
+        Match(title = 'branchdialog'),  # gitk
     ])
 auto_fullscreen = True
-focus_on_window_activation = "smart"
+focus_on_window_activation = 'smart'
 
 # Arquivo de Autostart.
-
-
-@ hook.subscribe.startup_once
+@hook.subscribe.startup_once
 def start_once():
-    subprocess.call([home + "/.config/qtile/scripts/autostart"])
+    subprocess.call([home + '/.config/qtile/scripts/autostart'])
 
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
@@ -295,4 +317,4 @@ def start_once():
 #
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
-wmname = "LG3D"
+wmname = 'LG3D'
