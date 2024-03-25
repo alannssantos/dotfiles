79005ea6689f96d4fd1ba170bfd5e3300f3b321a
Alann Santos <santos_alann@hotmail.com>
2024-03-25 12:17:22 -0300

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,68 +4,68 @@
 --   / ___ \| | (_| | | | | | | |
 --  /_/   \_\_|\__,_|_| |_|_| |_|
 
-vim.g.mapleader         = ' '
+vim.g.mapleader = " "
 
-if vim.fn.has('termguicolors') == 1 then
-    vim.g.termguicolors = true
+if vim.fn.has("termguicolors") == 1 then
+  vim.g.termguicolors = true
 end
 
-vim.opt.title           = true
-vim.opt.hidden          = true
-vim.opt.spell           = false
-vim.opt.number          = true
-vim.opt.confirm         = true
-vim.opt.hlsearch        = true
-vim.opt.wildmenu        = true
-vim.opt.expandtab       = true
-vim.opt.incsearch       = true
-vim.opt.path            = '+=**'
-vim.opt.splitbelow      = true
-vim.opt.splitright      = true
-vim.opt.smartindent     = true
-vim.opt.spelllang       = 'pt'
-vim.opt.laststatus      = 2
-vim.opt.shiftwidth      = 2
-vim.opt.showtabline     = 2
-vim.opt.softtabstop     = 2
-vim.opt.colorcolumn     = '80'
-vim.opt.foldmethod      = 'marker'
-vim.opt.clipboard       = 'unnamedplus'
-vim.opt.guifont         = 'Fira Code:h12'
-vim.opt.relativenumber  = true
+vim.opt.title = true
+vim.opt.hidden = true
+vim.opt.spell = false
+vim.opt.number = true
+vim.opt.confirm = true
+vim.opt.hlsearch = true
+vim.opt.wildmenu = true
+vim.opt.expandtab = true
+vim.opt.incsearch = true
+vim.opt.path = "+=**"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.smartindent = true
+vim.opt.spelllang = "pt"
+vim.opt.laststatus = 2
+vim.opt.shiftwidth = 2
+vim.opt.showtabline = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.colorcolumn = "80"
+vim.opt.foldmethod = "marker"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.guifont = "Fira Code:h12"
+vim.opt.relativenumber = true
 
 -- mapping
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("i", "kj", "<Esc>")
-vim.keymap.set("n", "<Tab>", ":bNext<CR>")
+vim.keymap.set("n", "<Tab>", ":bNext<CR>", {})
 vim.keymap.set("n", "<leader>e", ":Lexplore<CR>")
-vim.keymap.set("n", "<leader><Tab>", ":tabNext<CR>")
-vim.keymap.set('n', '<leader>n', ':%!nl -n rz -w3<CR>')
-vim.keymap.set("n", "<leader>tt", ":tabnew term://bash<CR>")
-vim.keymap.set('n', '<leader>o', ':set spell! spelllang=pt<CR>')
-vim.keymap.set('n', '<leader>s', ':!clear && shellcheck -x %<CR>')
+vim.keymap.set("n", "<leader><Tab>", ":tabNext<CR>", {})
+vim.keymap.set("n", "<leader>n", ":%!nl -n rz -w3<CR>", {})
+vim.keymap.set("n", "<leader>tt", ":tabnew term://bash<CR>", {})
+vim.keymap.set("n", "<leader>o", ":set spell! spelllang=pt<CR>", {})
+vim.keymap.set("n", "<leader>s", ":!clear && shellcheck -x %<CR>", {})
+vim.keymap.set("n", "<leader>q", ":bwipe<CR>", { desc = "Fechar Buffer" })
 
 -- netrw: config
-vim.g['netrw_altv']         = '1'
-vim.g['netrw_winsize']      = '25'
-vim.g['netrw_liststyle']    = '3'
-vim.g['netrw_browse_split'] = '3'
-vim.api.nvim_create_autocmd('filetype', {
-  pattern = 'netrw',
-  desc = 'Better mappings for netrw',
+vim.g["netrw_altv"] = "1"
+vim.g["netrw_winsize"] = "25"
+vim.g["netrw_liststyle"] = "3"
+vim.g["netrw_browse_split"] = "3"
+vim.api.nvim_create_autocmd("filetype", {
+  pattern = "netrw",
+  desc = "Better mappings for netrw",
   callback = function()
     local bind = function(lhs, rhs)
-      vim.keymap.set('n', lhs, rhs, {remap = true, buffer = true})
+      vim.keymap.set("n", lhs, rhs, { remap = true, buffer = true })
     end
     -- open file
-    bind('l', '<CR>')
-
+    bind("l", "<CR>")
     -- edit new file
-    bind('n', '%')
-
+    bind("n", "%")
     -- rename file
-    bind('r', 'R')
-  end
+    bind("r", "R")
+  end,
 })
 
 require("lazy-plugin")
