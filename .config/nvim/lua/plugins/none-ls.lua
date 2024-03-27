85807a261cd6495301866b4917b719bf13334079
Alann Santos <santos_alann@hotmail.com>
2024-03-27 16:32:17 -0300

    Update none-ls.lua

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -2,6 +2,7 @@ return {
   "nvimtools/none-ls.nvim",
   dependencies = {
     "nvimtools/none-ls-extras.nvim",
+    "gbprod/none-ls-shellcheck.nvim",
   },
   config = function()
     local null_ls = require("null-ls")
@@ -12,8 +13,11 @@ return {
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.shfmt,
         null_ls.builtins.diagnostics.pylint,
         require("none-ls.diagnostics.eslint_d"),
+        require("none-ls-shellcheck.diagnostics"),
+        require("none-ls-shellcheck.code_actions"),
         null_ls.builtins.completion.spell,
       },
     })
