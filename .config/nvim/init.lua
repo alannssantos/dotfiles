09569a7d043ffe93587fae3610a7900ab515a399
Alann Santos <santos_alann@hotmail.com>
2024-08-13 22:42:15 -0300

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,44 +7,55 @@
 vim.g.mapleader = " "
 
 if vim.fn.has("termguicolors") == 1 then
-  vim.g.termguicolors = true
+	vim.g.termguicolors = true
 end
 
+vim.opt.list = true
+vim.opt.tabstop = 2
 vim.opt.title = true
-vim.opt.hidden = true
 vim.opt.spell = false
+vim.opt.hidden = true
 vim.opt.number = true
-vim.opt.confirm = true
-vim.opt.hlsearch = true
-vim.opt.wildmenu = true
-vim.opt.expandtab = true
-vim.opt.incsearch = true
 vim.opt.path = "+=**"
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.smartindent = true
-vim.opt.spelllang = "pt"
+vim.opt.confirm = true
+vim.opt.scrolloff = 10
 vim.opt.laststatus = 2
 vim.opt.shiftwidth = 2
+vim.opt.hlsearch = true
+vim.opt.wildmenu = true
 vim.opt.showtabline = 2
 vim.opt.softtabstop = 2
-vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.incsearch = true
+vim.opt.spelllang = "pt"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 vim.opt.colorcolumn = "80"
+vim.opt.smartindent = true
 vim.opt.foldmethod = "marker"
-vim.opt.clipboard = "unnamedplus"
-vim.opt.guifont = "Fira Code:h12"
 vim.opt.relativenumber = true
+vim.opt.guifont = "Fira Code:h12"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
 
 -- mapping
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("i", "kj", "<Esc>")
 vim.keymap.set("n", "<Tab>", ":tabNext<CR>", {})
 vim.keymap.set("n", "<leader>e", ":Lexplore<CR>")
+vim.keymap.set("n", "<leader>tt", ":silent !tmux splitw -vd -p 30<CR>", {})
 vim.keymap.set("n", "<leader><Tab>", ":bNext<CR>", {})
 vim.keymap.set("n", "<leader>n", ":%!nl -n rz -w3<CR>", {})
 vim.keymap.set("n", "<leader>o", ":set spell! spelllang=pt<CR>", {})
 vim.keymap.set("n", "<leader>q", ":bwipe<CR>", { desc = "Fechar Buffer" })
-vim.keymap.set("n", "<leader>tt", ":silent !tmux splitw -vd -p 30<CR>", {})
 
 -- netrw: config
 vim.g["netrw_altv"] = "1"
@@ -53,19 +64,19 @@ vim.g["netrw_liststyle"] = "3"
 vim.g["netrw_browse_split"] = "3"
 vim.g["netrw_list_hide"] = "\\(^\\|\\s\\s\\)\\zs\\.\\S\\+"
 vim.api.nvim_create_autocmd("filetype", {
-  pattern = "netrw",
-  desc = "Better mappings for netrw",
-  callback = function()
-    local bind = function(lhs, rhs)
-      vim.keymap.set("n", lhs, rhs, { remap = true, buffer = true })
-    end
-    -- open file
-    bind("l", "<CR>")
-    -- edit new file
-    bind("f", "%")
-    -- rename file
-    bind("r", "R")
-  end,
+	pattern = "netrw",
+	desc = "Better mappings for netrw",
+	callback = function()
+		local bind = function(lhs, rhs)
+			vim.keymap.set("n", lhs, rhs, { remap = true, buffer = true })
+		end
+		-- open file
+		bind("l", "<CR>")
+		-- edit new file
+		bind("f", "%")
+		-- rename file
+		bind("r", "R")
+	end,
 })
 
 require("lazy-plugin")
