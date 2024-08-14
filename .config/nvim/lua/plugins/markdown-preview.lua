8976d8d6f9fe1cfd0efd72a3d1da9664f9bbc87c
Alann Santos <santos_alann@hotmail.com>
2024-08-13 22:22:42 -0300

    Delete .config/nvim/lua/plugins directory

--- .config/nvim/lua/plugins/markdown-preview.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "iamcco/markdown-preview.nvim",
-  ft = "markdown",
-  config = function()
-    vim.fn["mkdp#util#install"]()
-    vim.keymap.set("n", "gm", ":MarkdownPreview<CR>", { desc = "Markdown Preview" })
-  end,
-}
