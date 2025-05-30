e02e66834389a9ce2402311f202ffc794f798323
Alann Santos <santos_alann@hotmail.com>
2025-05-30 13:39:56 -0300

    Update init.lua

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,1 +1,9 @@
 require("full-border"):setup()
+require("gvfs"):setup({
+	-- (Optional) Allowed keys to select device.
+	which_keys = "1234567890qwertyuiopasdfghjklzxcvbnm-=[]\\;',./!@#$%^&*()_+{}|:\"<>?",
+
+	-- (Optional) Save file.
+	-- Default: ~/.config/yazi/gvfs.private
+	save_path = os.getenv("HOME") .. "/.config/yazi/gvfs.private",
+})
