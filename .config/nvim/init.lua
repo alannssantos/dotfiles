d467caa81398cae5ea368d77a3464fa2273ae491
Alann Santos <santos_alann@hotmail.com>
2025-08-06 02:49:47 +0000

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,34 +10,53 @@ if vim.fn.has("termguicolors") == 1 then
 	vim.g.termguicolors = true
 end
 
-vim.opt.list = true
-vim.opt.tabstop = 2
-vim.opt.title = true
-vim.opt.spell = false
-vim.opt.hidden = true
-vim.opt.number = true
-vim.opt.path = "+=**"
-vim.opt.confirm = true
-vim.opt.scrolloff = 10
-vim.opt.laststatus = 2
-vim.opt.shiftwidth = 2
-vim.opt.hlsearch = true
-vim.opt.wildmenu = true
-vim.opt.showtabline = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
-vim.opt.incsearch = true
-vim.opt.spelllang = "pt"
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.colorcolumn = "80"
-vim.opt.smartindent = true
-vim.opt.foldmethod = "marker"
-vim.opt.relativenumber = true
-vim.opt.guifont = "Fira Code:h11"
-vim.opt.clipboard = "unnamedplus"
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+-- basics settings
+local set = vim.opt
+set.list = true
+set.tabstop = 2
+set.title = true
+set.spell = false
+set.hidden = true
+set.number = true
+set.path = "+=**"
+set.confirm = true
+set.scrolloff = 10
+set.laststatus = 2
+set.shiftwidth = 2
+set.hlsearch = true
+set.wildmenu = true
+set.showtabline = 2
+set.softtabstop = 2
+set.expandtab = true
+set.incsearch = true
+set.spelllang = "pt"
+set.splitbelow = true
+set.splitright = true
+set.colorcolumn = "80"
+set.smartindent = true
+set.winborder = "rounded"
+set.foldmethod = "marker"
+set.relativenumber = true
+set.guifont = "Fira Code:h11"
+set.clipboard = "unnamedplus"
+set.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
+-- mapping
+local map = vim.keymap
+map.set("i", "jk", "<Esc>")
+map.set("i", "kj", "<Esc>")
+map.set("t", "jk", "<C-\\><C-n>")
+map.set("t", "kj", "<C-\\><C-n>")
+map.set("t", "<Esc>", "<C-\\><C-n>")
+map.set("n", "<Tab>", ":bNext<CR>", {})
+map.set("n", "<leader>e", ":Lexplore<CR>")
+map.set("n", "<leader><Tab>", ":tabNext<CR>", {})
+map.set("n", "<leader>t", ":terminal ", {})
+map.set("n", "<leader>n", ":%!nl -n rz -w3<CR>", {})
+map.set("n", "<leader>o", ":set spell! spelllang=pt<CR>", {})
+map.set("n", "<leader>q", ":bwipe<CR>", { desc = "Fechar Buffer" })
+
+-- blink on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
@@ -45,20 +64,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
-
--- mapping
-vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("i", "kj", "<Esc>")
-vim.keymap.set("t", "jk", "<C-\\><C-n>")
-vim.keymap.set("t", "kj", "<C-\\><C-n>")
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
-vim.keymap.set("n", "<Tab>", ":bNext<CR>", {})
-vim.keymap.set("n", "<leader>e", ":Lexplore<CR>")
-vim.keymap.set("n", "<leader><Tab>", ":tabNext<CR>", {})
-vim.keymap.set("n", "<leader>t", ":terminal ", {})
-vim.keymap.set("n", "<leader>n", ":%!nl -n rz -w3<CR>", {})
-vim.keymap.set("n", "<leader>o", ":set spell! spelllang=pt<CR>", {})
-vim.keymap.set("n", "<leader>q", ":bwipe<CR>", { desc = "Fechar Buffer" })
 
 -- netrw: config
 vim.g["netrw_altv"] = "1"
